781044aebf15240a604874ce48fa2dcd51711555
Nhu Anh Vu <vunhuanh@gmail.com>
2021-02-08 09:35:37 -0500

    setup docker, docker-compose, flask

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/get_game')
+def get_game():
+    return 'lebron'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
